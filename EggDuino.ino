6e4923da63e2ffc55f3e5f861493332b40ec7b88
StefaniGit <StefaniGit@knallakoff.de>
2018-02-04 20:53:42 +0100

    Arduino IDE 1.8.2 ans Eggbot Inkscape 2.3 compatible

--- EggDuino.ino
+++ EggDuino.ino
@@ -27,15 +27,17 @@
 
 #define initSting "EBBv13_and_above Protocol emulated by Eggduino-Firmware V1.6a"
 //Rotational Stepper:
-#define step1 11
-#define dir1 10
-#define enableRotMotor 9
+#define step1 2 //OA was: 11
+#define dir1 5 //OA was: 10
+#define enableRotMotor 8 //OA was: 9; probably not used with Arduino
 #define rotMicrostep 16  //MicrostepMode, only 1,2,4,8,16 allowed, because of Integer-Math in this Sketch
+                        //OA default was 16
 //Pen Stepper:
-#define step2 8
-#define dir2 7
-#define enablePenMotor 6
+#define step2 4 //OA was: 8
+#define dir2 7 //OA was: 7
+#define enablePenMotor 8 //OA was: 6; probably not used with Arduino
 #define penMicrostep 16 //MicrostepMode, only 1,2,4,8,16 allowed, because of Integer-Math in this Sketch
+                       // default was 16
 
 #define servoPin 3 //Servo
 
@@ -88,6 +90,7 @@ void setup() {
 	Serial.begin(9600);
 	makeComInterface();
 	initHardware();
+  Serial.print ("hi");
 }
 
 void loop() {
